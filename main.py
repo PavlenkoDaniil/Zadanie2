b60b2395cd6113e0d9becd702d3f1c14843be09d
PavlenkoDaniil <danpavalex@mail.ru>
2024-06-01 16:27:39 +0500

    Zadanie3

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtGui import QPainter, QColor, QPolygon
 import sys
 import random
 from UI import Ui_MainWindow
+
+SCREEN_SIZE = [680, 480]
+
 
 class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.flag = False
+        self.setWindowTitle('Супрематизм')
         self.pushButton.clicked.connect(self.draw)
-        self.circle_x = None
-        self.circle_y = None
-        self.circle_size = None
+        self.coords = []
 
     def draw(self):
-        self.circle_x = 150
-        self.circle_y = 150
-        self.circle_size = 40
+        self.figure = 'circle'
+        self.size = random.randint(10, 100)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  # 'yellow'
+        self.flag = True
         self.update()
 
     def paintEvent(self, event):
-        if self.circle_x is not None and self.circle_y is not None and self.circle_size is not None:
+        if self.flag:
             qp = QPainter()
             qp.begin(self)
-            qp.setPen(QColor(255, 255, 0))  # жёлтый цвет
-            qp.setBrush(QColor(255, 255, 0))  # жёлтый цвет
-            qp.drawEllipse(self.circle_x, self.circle_y, self.circle_size, self.circle_size)
+            qp.setPen(QColor(*self.color))
+            qp.setBrush(QColor(*self.color))
+            self.x, self.y = random.randint(100, SCREEN_SIZE[0] - 100), random.randint(100, SCREEN_SIZE[1] - 100)
+            if self.figure == 'circle':
+                qp.drawEllipse(self.x, self.y, self.size, self.size)
             qp.end()
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    sys.excepthook = except_hook
     ex = Example()
     ex.show()
     sys.exit(app.exec_())
